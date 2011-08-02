c701196f2a79dada8afc6090ae2e23abe1c0029a
jonezy <chris@jonezy.org>
2011-08-01 21:33:59 -0400

    updated the reset password process a little bit

--- Epilogger.Web/Models/UpdatePasswordModel.cs
+++ Epilogger.Web/Models/UpdatePasswordModel.cs
@@ -1,7 +1,13 @@
-﻿namespace Epilogger.Web.Models {
+﻿using System.ComponentModel;
+namespace Epilogger.Web.Models {
     public class UpdatePasswordModel {
+        [DisplayName("Email Address (you'll get an email to this address confirming the new password (NOT WORKING RIGHT NOW))")]
         public string EmailAddress { get; set; }
+
+        [DisplayName("New password")]
         public string NewPassword { get; set; }
+
+        [DisplayName("New password again")]
         public string NewPasswordAgain { get; set; }
     }
 }
