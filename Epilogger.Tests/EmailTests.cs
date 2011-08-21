b2680a7c0e64eb8ce36cf3377beffc909c871925
jonezy <chris@jonezy.org>
2011-08-21 10:51:58 -0400

    adding a test project to help with some of those hard to build features.  To run locally instll the nunit gui.

---
+++ Epilogger.Tests/EmailTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Epilogger.Web.Core.Email;
+using NUnit.Framework;
+
+namespace Epilogger.Tests {
+    public class EmailTests {
+        public void replaced_string_should_match() {
+            string messageTemplate = "Hi, [FIRST_NAME].  This is a message template";
+            string expected = "Hi, Chris.  This is a message template";
+            
+            Dictionary<string, string> replacements = new Dictionary<string,string>();
+            replacements.Add("[FIRST_NAME]", "Chris");
+                
+            TemplateParser parser = new TemplateParser();
+            string actual = parser.Parse(messageTemplate, replacements);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Epilogger.Web/Core/Email/TemplateParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epilogger.Web.Core.Email {
+    
+    public class TemplateParser {
+        
+        /// <summary>
+        /// Matches and replaces tokens in messageTemplate with values in replacements.
+        /// </summary>
+        /// <param name="messageTemplate">string containing tokens to be replaced ([EX_TOKEN]).</param>
+        /// <param name="replacements">Dictionary of replacements</param>
+        /// <returns>messageTemplate with it's tokens replaced</returns>
+        public string Parse(string messageTemplate, Dictionary<string, string> replacements) {
+            string returnValue = "";
+            
+            foreach (KeyValuePair<string, string> replacement in replacements) {
+                if (messageTemplate.IndexOf(replacement.Key) >= 0) {
+                    returnValue += messageTemplate.Replace(replacement.Key, replacement.Value);
+                }
+            }
+
+            return returnValue;
+        }
+
+    }
+}
