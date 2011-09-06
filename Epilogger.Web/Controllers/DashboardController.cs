4c01f7c897297f05021d8fe5fd56b9ea1e73c5ae
jonezy <chris@jonezy.org>
2011-09-06 19:35:48 -0400

    Updated event details layout, starting to add dashboard functionality
    - accidently borked the details page by adding padding to the container, fixed now.
    - starting to wire up the basics for the dashbaoard functionality

---
+++ Epilogger.Web/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Epilogger.Web.Controllers {
+    [RequiresAuthentication(AccessDeniedMessage = "You must be logged in to view your dashboard")]
+    public class DashboardController : BaseController {
+
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
