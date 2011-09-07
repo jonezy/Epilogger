4bc652e6c8018e8b5c2190c4374320cc7b1710fd
jonezy <chris@jonezy.org>
2011-09-06 20:21:31 -0400

    Refactored some of ServiceBase to resolve query issues
    - saw the issues brooker was concerned about, included a temporary fix that i'll need for the dashboard section.  It's an overloaded GetData method in ServiceBase that accepts a query expression and then executes that
    
    - removed the need to pass db into the GetRepository() method (totally not required and pointless)

--- Epilogger.Web/Controllers/DashboardController.cs
+++ Epilogger.Web/Controllers/DashboardController.cs
@@ -1,10 +1,22 @@
 ﻿using System.Web.Mvc;
+using System.Collections;
+using Epilogger.Data;
+using System.Collections.Generic;
 
 namespace Epilogger.Web.Controllers {
     [RequiresAuthentication(AccessDeniedMessage = "You must be logged in to view your dashboard")]
     public class DashboardController : BaseController {
+        TweetService tweetService;
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext) {
+            if (tweetService == null) tweetService = new TweetService();
+
+            base.Initialize(requestContext);
+        }
 
         public ActionResult Index() {
+            IEnumerable<Tweet> tweets = tweetService.FindByUserScreenName(CurrentUserTwitterAuthorization.ServiceUsername);
+
             return View();
         }
 
