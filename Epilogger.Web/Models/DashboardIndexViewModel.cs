05a23043cd04ea28bfd338e5a8d7ffc398cbd8cd
jonezy <chris@jonezy.org>
2011-09-07 20:22:50 -0400

    Introduced a simple naive pager (see the dashboard index)
    - Added an implementation of the pager code from RichmondDay.Helpers to the site.  It works pretty well and is easy to implement but it's very limited in one way.  It requires that you pass the entire collection into the control which won't scale in our case sometimes (or will it, as in many cases we are getting the complete data set anyways?) ah well good enough for now.

--- Epilogger.Web/Models/DashboardIndexViewModel.cs
+++ Epilogger.Web/Models/DashboardIndexViewModel.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+
+using System.Web.Mvc;
+using RichmondDay.Helpers;
 
 namespace Epilogger.Web.Models {
     public class DashboardIndexViewModel {
-        public List<DashboardActivityModel> Activity { get; set; }
+        public int PageSize { get { return 12; } }
+        public int CurrentPageIndex { get; set; }
+        public int TotalRecords { get; set; }
+
+        public string TotalRecordsLabel { get; set; }
+
+        public IPagedList<DashboardActivityModel> Activity { get; set; }
+
+        public DashboardIndexViewModel(IList<DashboardActivityModel> activities, int currentPageIndex) {
+            CurrentPageIndex = currentPageIndex;
+            TotalRecords = activities.Count;
+
+            Activity = activities.ToPagedList(CurrentPageIndex, PageSize, TotalRecords);
+        }
     }
 }
