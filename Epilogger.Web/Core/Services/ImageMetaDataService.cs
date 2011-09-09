d23a92f95e44e39120754ced64e59a91b95d9e13
Chris Brooker <cbrooker@gmail.com>
2011-09-09 16:43:18 -0400

    Forgot Files

---
+++ Epilogger.Web/Core/Services/ImageMetaDataService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using Epilogger.Data;
+using Epilogger.Web.Model;
+
+namespace Epilogger.Web
+{
+    public class ImageMetaDateService : ServiceBase<ImageMetaDatum>
+    {
+        protected override string CacheKey
+        {
+            get { return "Epilogger.Web.ImageMetaData"; }
+        }
+
+        public List<ImageMetaDatum> FindByEventID(int EventID)
+        {
+            return FindByEventID(EventID, DateTime.Parse("1900-01-01 00:00:00"), DateTime.Parse("9999-12-31 00:00:00"));
+        }
+
+        public List<ImageMetaDatum> FindByEventID(int EventID, DateTime StartDateTimeFilter, DateTime EndDateTimeFilter)
+        {
+            return GetData(t => t.EventID == EventID & t.DateTime >= StartDateTimeFilter & t.DateTime <= EndDateTimeFilter);
+        }
+
+        public List<ImageMetaDatum> FindByImageID(int ImageID)
+        {
+            return FindByImageID(ImageID, DateTime.Parse("1900-01-01 00:00:00"), DateTime.Parse("9999-12-31 00:00:00"));
+        }
+
+        public List<ImageMetaDatum> FindByImageID(int ImageID, DateTime StartDateTimeFilter, DateTime EndDateTimeFilter)
+        {
+            return GetData(i => i.ImageID == ImageID & i.DateTime >= StartDateTimeFilter & i.DateTime <= EndDateTimeFilter);
+        }
+
+
+    }
+}
