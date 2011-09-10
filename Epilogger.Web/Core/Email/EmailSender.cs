a54c20d1a46e98940cfbb59c21c8a6a538b8d51a
jonezy <chris@jonezy.org>
2011-09-09 23:30:34 -0400

    renamed mispelled file

---
+++ Epilogger.Web/Core/Email/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Epilogger.Web.Core.Email {
+    public class EmailSender : IEmailSender {
+        public void Send(SmtpConfiguration smtpConfig, string toAddress, string fromAddress, string subject, string message) {
+            // have to add this check to RichmondDay.Helpers
+            //if(smtpConfig.IsEmpty())
+            if (smtpConfig == null || string.IsNullOrEmpty(smtpConfig.Server) || string.IsNullOrEmpty(smtpConfig.Port.ToString())) {
+                throw new ArgumentException("smtpConfig");
+            }
+
+            // if there is no from address specified, use the default one.
+            if (string.IsNullOrEmpty(fromAddress))
+                fromAddress = smtpConfig.DefaultFromAddress;
+
+            if (string.IsNullOrEmpty(toAddress))
+                toAddress = smtpConfig.DefaultToAddress;
+
+            // setup the message
+            MailMessage email = new MailMessage();
+            email.Subject = subject;
+            email.Body = message;
+            email.From = new MailAddress(fromAddress);
+            email.To.Add(new MailAddress(toAddress));
+            email.IsBodyHtml = true;
+
+            // setup the smtpclient 
+            // only need to add credentials if they are present (production)
+            SmtpClient client = new SmtpClient(smtpConfig.Server, smtpConfig.Port);
+            if (!string.IsNullOrEmpty(smtpConfig.Username) && !string.IsNullOrEmpty(smtpConfig.Password)) {
+                client.Credentials = new System.Net.NetworkCredential(smtpConfig.Username, smtpConfig.Password);
+            }
+            
+            // send the message
+            client.Send(email);
+        }
+    }
+}
