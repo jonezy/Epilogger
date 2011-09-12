6b4ac0fcc2dbda2cbb73b2f86fc6f8443b9081c9
Chris Brooker <cbrooker@gmail.com>
2011-09-12 17:20:43 -0400

    All Photos page is up.

--- Epilogger.Web/Models/AllPhotosDisplayViewModel.cs
+++ Epilogger.Web/Models/AllPhotosDisplayViewModel.cs
@@ -8,10 +8,15 @@ namespace Epilogger.Web.Models
 {
     public class AllPhotosDisplayViewModel
     {
+        public string ID { get; set; }
         public string Name { get; set; }
+
+        public bool ShowTopPhotos { get; set; }
+
         public int PhotoCount { get; set; }
         public int Page { get; set; }
-
+        public int CurrentPageIndex { get; set; }
+        
         public IEnumerable<Epilogger.Data.Image> Images { get; set;}
         
     }
