ec25d13d31cb518b668f2ba3dfb2611bfe4a7844
jonezy <chris@jonezy.org>
2011-09-24 07:29:02 -0400

    a bit of cleanup in the events controller, just a little.

--- Epilogger.Web/Core/Stats/TopTweeters.cs
+++ Epilogger.Web/Core/Stats/TopTweeters.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-
-using Epilogger.Data;
 
 namespace Epilogger.Web.Core.Stats {
     public class TopTweetersStats {
-
-
         public List<Tweeter> Calculate(IEnumerable<Tweeter> tweeters) {
             List<Tweeter> topTweeters = new List<Tweeter>();
             // first loop through all the results and get a total
