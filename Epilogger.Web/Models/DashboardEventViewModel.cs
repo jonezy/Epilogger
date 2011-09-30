d3312f06f5560535c2c41f88c75cd3ef745e4e14
jonezy <chris@jonezy.org>
2011-09-30 00:29:11 -0400

    fixed issue with the going on now page (again)

--- Epilogger.Web/Models/DashboardEventViewModel.cs
+++ Epilogger.Web/Models/DashboardEventViewModel.cs
@@ -5,6 +5,7 @@ namespace Epilogger.Web.Models {
         public int ID { get; set; }
         public string Name { get; set; }
         public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
         public string SearchTerms { get; set; }
         public int TotalTweets { get; set; }
         public int TotalMedia { get; set; }
