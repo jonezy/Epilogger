f86bea716d93f8bb00c1a5ee08b6f826932a9bd5
Chris Brooker <cbrooker@gmail.com>
2011-10-03 22:53:14 -0400

    Overhaul of the whole timezone system. Need to deploy to test.

---
+++ Epilogger.Web/Core/Framework/DateTimeModelBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Timezone.Framework
+{
+
+    public class DateTimeModelBinder : IModelBinder
+    {
+        
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (value == null || value.AttemptedValue == "null" || value.AttemptedValue == String.Empty) return null;
+            return TimeZoneManager.ToUtcTime(((DateTime?)value.ConvertTo(typeof(DateTime))).Value);
+        }
+
+    }
+
+}
---
+++ Epilogger.Web/Core/Framework/TimezoneManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Timezone.Framework
+{
+
+    public class TimeZoneManager
+    {
+
+        private const string UserTimezoneContextKey = "USER_TIMEZONE";
+
+        public static void UpdateTimeZone(HttpRequest request)
+        {
+            //Get the browser timezone from the cookie
+            var cookie = request.Cookies["TimeZoneOffset"];
+            if (cookie == null) return;
+            var offset = new TimeSpan(0, Int32.Parse(cookie.Value), 0);
+            UserTimeZone = TimeZoneInfo.CreateCustomTimeZone("Browser", offset, "Browser", "Browser");
+        }
+
+        public static TimeZoneInfo UserTimeZone
+        {
+            get
+            {
+                var contextValue = HttpContext.Current.Items[UserTimezoneContextKey];
+                return (contextValue == null) ? TimeZoneInfo.Local : (TimeZoneInfo)contextValue;
+            }
+            set
+            {
+                HttpContext.Current.Items[UserTimezoneContextKey] = value;
+            }
+        }
+
+        public static DateTime GetUserTime(DateTime value)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(value, UserTimeZone);
+        }
+
+        public static DateTime ToUtcTime(DateTime value)
+        {
+            return TimeZoneInfo.ConvertTimeToUtc(value, UserTimeZone);
+        }
+
+    }
+
+}
