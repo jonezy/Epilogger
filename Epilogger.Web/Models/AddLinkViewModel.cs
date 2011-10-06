76ed471ebdcbab7ec05006b95b51a7af1ef3b923
jonezy <chris@jonezy.org>
2011-10-05 22:34:26 -0400

    accidently worked in master ;( stubbed in the add blog post and add link windows

---
+++ Epilogger.Web/Models/AddLinkViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Epilogger.Web.Models {
+    public class AddLinkViewModel {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Caption { get; set; }
+    }
+}
