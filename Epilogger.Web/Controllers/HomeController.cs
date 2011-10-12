ed335d2a5bb55573d1a8e027e7a1efa123379d0b
jonezy <chris@jonezy.org>
2011-10-12 19:46:29 -0400

    finished up the site footer

--- Epilogger.Web/Controllers/HomeController.cs
+++ Epilogger.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System.Web.Mvc;
-using System;
-using Epilogger.Web.Models;
+﻿using System;
 using System.Collections.Generic;
+using System.Web.Mvc;
+
 using AutoMapper;
+
+using Epilogger.Web.Models;
 
 namespace Epilogger.Web.Controllers {
     public class HomeController : BaseController {
@@ -10,34 +12,25 @@ namespace Epilogger.Web.Controllers {
         ClickLogService CS = new ClickLogService();
         EventService ES = new EventService();
 
-
         public ActionResult Index() {
             ViewBag.Message = "Welcome to Epilogger!";
-            
+
             return View();
         }
-
-        public ActionResult About() {
-            return View();
-        }
-
 
         [HttpPost]
-        public ActionResult LogClick(Epilogger.Data.UserClickAction clickactions)
-        {
+        public ActionResult LogClick(Epilogger.Data.UserClickAction clickactions) {
 
             clickactions.ActionDateTime = DateTime.UtcNow;
             clickactions.UserID = CurrentUserID;
-            
+
             LS.Save(clickactions);
-            
+
             return Json(new { success = true });
         }
 
-
         [HttpPost]
-        public ActionResult ClickMap(Epilogger.Data.userClickTracking clickactions)
-        {
+        public ActionResult ClickMap(Epilogger.Data.userClickTracking clickactions) {
 
             clickactions.UserID = CurrentUserID;
             clickactions.ClickDateTime = DateTime.UtcNow;
@@ -47,28 +40,38 @@ namespace Epilogger.Web.Controllers {
             return Json(new { success = true });
         }
 
-        public ActionResult _GetClickMap(Epilogger.Data.userClickTracking clickactions)
-        {
+        public ActionResult _GetClickMap(Epilogger.Data.userClickTracking clickactions) {
             return PartialView(CS.GetLast200ClicksByLocation(clickactions.location));
         }
 
-
-        public ActionResult Search()
-        {
+        public ActionResult Search() {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Search(SearchEventViewModel model)
-        {
+        public ActionResult Search(SearchEventViewModel model) {
 
             IEnumerable<Epilogger.Data.Event> Evs = ES.GetEventsBySearchTerm(model.SearchTerm);
 
             model.Events = Mapper.Map<IEnumerable<Epilogger.Data.Event>, List<DashboardEventViewModel>>(Evs);
-            
+
             return View(model);
         }
 
+        public ActionResult About() {
+            return View();
+        }
 
+        public ActionResult Contact() {
+            return View();
+        }
+
+        public ActionResult Terms() {
+            return View();
+        }
+        
+        public ActionResult Privacy() {
+            return View();
+        }
     }
 }
