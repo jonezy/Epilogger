d3a20d434b86e56126ae9ef3a2f00b0226182a56
Chris Brooker <cbrooker@gmail.com>
2011-10-15 14:36:43 -0400

    Almost all Venue search stuff has been tweaked and fixed

--- Epilogger.Web/Models/FoursquareVenue.cs
+++ Epilogger.Web/Models/FoursquareVenue.cs
@@ -3,5 +3,8 @@
         public string id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
     }
 }
