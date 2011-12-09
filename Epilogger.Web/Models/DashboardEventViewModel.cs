83d55491a64cbc0343dc24dc4013df9536545de8
Chris Jones <chris@jonezy.org>
2011-12-08 21:50:33 -0500

    random event is done

--- Epilogger.Web/Models/DashboardEventViewModel.cs
+++ Epilogger.Web/Models/DashboardEventViewModel.cs
@@ -4,6 +4,7 @@ namespace Epilogger.Web.Models {
     public class DashboardEventViewModel {
         public int ID { get; set; }
         public int EventCategoryID { get; set; }
+        public string Description { get; set; }
         public string Name { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
