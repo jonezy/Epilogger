508391b8499384fde2f8814919c49688acb2a170
Chris Brooker <cbrooker@gmail.com>
2011-12-08 22:20:32 -0500

    Edit page updated

--- Epilogger.Web/Public/js/CreateEditEvent.js
+++ Epilogger.Web/Public/js/CreateEditEvent.js
@@ -12,58 +12,47 @@ head.ready(function () {
     //$("#step1").removeClass("stepDisable"); 
 
     //Block the Other sections
-    $("#whattoinclude-info .blocking").block({ message: null });
-    $("#whattoinclude-info h4").block({ message: null });
+    if (page == 'Create') {
+        $("#whattoinclude-info .blocking").block({ message: null });
+        $("#whattoinclude-info h4").block({ message: null });
 
-    $("#whenandwhere-info .blocking").block({ message: null });
-    $("#whenandwhere-info h4").block({ message: null });
+        $("#whenandwhere-info .blocking").block({ message: null });
+        $("#whenandwhere-info h4").block({ message: null });
 
-    $("#moreinformation-info .blocking").block({ message: null });
-    $("#moreinformation-info h4").block({ message: null });
+        $("#moreinformation-info .blocking").block({ message: null });
+        $("#moreinformation-info h4").block({ message: null });
 
-    $(".submitForm").attr('disabled', 'disabled');
-    //$(".disabled").block({ message: null });
-
-
-
-    $(":input", "#createevent-info").not(":button, :submit, :reset, :hidden").bind("blur", function (e) {
-        checkBlocking();
-    });
-    $("#CategoryID").bind("change", function (e) {
-        checkBlocking();
-    });
-
-    $(":input", "#whattoinclude-info").not(":button, :submit, :reset, :hidden").bind("keyup", function (e) {
-        checkBlocking();
-    });
-    var showMore = false;
-    $(":input", "#whenandwhere-info").not(":button, :submit, :reset, :hidden").bind("click", function (e) {
-        showMore = true;
-        checkBlocking();
-    });
-    $(":input", "#moreinformation-info").not(":button, :submit, :reset, :hidden").bind("keyup", function (e) {
-        checkBlocking();
-    });
-
-    $("#AdvancedToggle").bind("click", function (e) {
-        e.preventDefault();
-        $("#AdvancedSearchTerms").toggle();
-    });
+        $(".submitForm").attr('disabled', 'disabled');
 
 
+
+        $(":input", "#createevent-info").not(":button, :submit, :reset, :hidden").bind("blur", function (e) {
+            checkBlocking();
+        });
+        $("#CategoryID").bind("change", function (e) {
+            checkBlocking();
+        });
+
+        $(":input", "#whattoinclude-info").not(":button, :submit, :reset, :hidden").bind("keyup", function (e) {
+            checkBlocking();
+        });
+        var showMore = false;
+        $(":input", "#whenandwhere-info").not(":button, :submit, :reset, :hidden").bind("click", function (e) {
+            showMore = true;
+            checkBlocking();
+        });
+        $(":input", "#moreinformation-info").not(":button, :submit, :reset, :hidden").bind("keyup", function (e) {
+            checkBlocking();
+        });
+
+        $("#AdvancedToggle").bind("click", function (e) {
+            e.preventDefault();
+            $("#AdvancedSearchTerms").toggle();
+        });
+
+    }
 
     function checkBlocking() {
-        //        var i = 0;
-        //        $(":input", "#createevent-info").not(":button, :submit, :reset, :hidden").each(function (index) {
-        //            if ($(this).val().length > 0) {
-        //                i++;
-        //            }
-        //        });
-        //        if (i == 2) {
-        //            $("#whattoinclude-info .blocking").unblock();
-        //            $("#whattoinclude-info h4").unblock();
-        //            $("#step2").removeClass("stepDisable");
-        //        }
 
         //Add the Complete style
         if ($("#Name").val().length > 0 && $("#CategoryID").val() != 0) {
@@ -159,6 +148,39 @@ head.ready(function () {
             $("#rule-group .rule .minus").addClass("disabled");
         }
     }
+
+
+
+//    function buildSearchBoxes() {
+//        //Take the value from the DB and parse it into all the correct search boxes
+//        
+
+//        
+//        counter++;
+
+//        var newItem = $("#template").clone();
+//        newItem.removeClass("hidden");
+//        newItem.insertAfter(this).attr('id', 'rule' + counter);
+//        $(':input', '#rule' + counter)
+//			.not(':button, :submit, :reset, :hidden')
+//			.val('')
+//			.removeAttr('checked')
+//			.removeAttr('selected')
+//            .attr("id", "seachTerm" + counter);
+
+//        $("select", "#rule" + counter).attr("id", "operator" + counter);
+
+//        $("#seachTerm" + counter).bind("keyup", function (e) {
+//            buildSearchString();
+//        });
+//        
+//    }
+
+
+
+
+
+
 
     $(":input", "#rule-group").not(":button, :submit, :reset, :hidden").bind("keyup", function (e) {
         buildSearchString();
