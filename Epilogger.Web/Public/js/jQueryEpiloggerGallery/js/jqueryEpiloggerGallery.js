c96bb017818d75b62bbbb371d440f47d14a91268
Chris Brooker <cbrooker@gmail.com>
2012-05-19 12:20:10 -0400

    Improving and testing image uplaod

--- Epilogger.Web/Public/js/jQueryEpiloggerGallery/js/jqueryEpiloggerGallery.js
+++ Epilogger.Web/Public/js/jQueryEpiloggerGallery/js/jqueryEpiloggerGallery.js
@@ -1,36 +1,77 @@
 ﻿(function ($) {
     $.fn.gallery = function (images, selectOnClick) {
         return this.each(function () {
-            var imagesPerColumn = 6;
+            var imagesPerColumn = 5; //6
 
-            var html = '<tr>';
-            var currentRowImagesCount = 0;
+            var html = '';
             $(images).each(function () {
-                html += '<td>';
-                if (!selectOnClick)
-                    html += '<a href=' + this.url + '>';
-                html += '<img width="100" src="' + this.src + '" class="unselectedImage"/>';
-                if (!selectOnClick)
-                    html += '</a>';
-                html += '</td>';
+                html += '<li>';
+                html += '<div class="facebookGalleryImg unselectedImage">';
+                        if (!selectOnClick)
+                            html += '<a href=' + this.url + '>';
 
-                currentRowImagesCount++;
+                        html += '<img width="180" src="' + this.src + '" />';
+                    html += '</div>';
 
-                if (currentRowImagesCount == imagesPerColumn) {
-                    html += "</tr><tr>";
-                    currentRowImagesCount = 0;
-                }
+                    html += '<div class="facebookGalleryAlbumName">' + this.albumname + '</div>';
+                    if (!selectOnClick)
+                        html += '</a>';
+
+                    html += '</li>';
+            
             });
-
-            html += '</tr>';
 
             $(this).html(html);
 
             if (selectOnClick) {
                 $(this).find('img').click(function () {
-                    $(this).toggleClass('selectedImage').toggleClass('unselectedImage');
+                    $(this).parent().toggleClass('selectedImage').toggleClass('unselectedImage');
                 });
             }
         });
     };
 })(jQuery);
+
+
+
+
+
+//(function ($) {
+//    $.fn.gallery = function (images, selectOnClick) {
+//        return this.each(function () {
+//            var imagesPerColumn = 5; //6
+
+//            var html = '<tr>';
+//            var currentRowImagesCount = 0;
+//            $(images).each(function () {
+//                html += '<td>';
+//                html += '<div>';
+//                if (!selectOnClick)
+//                    html += '<a href=' + this.url + '>';
+//                html += '<img width="100" src="' + this.src + '" class="unselectedImage"/>';
+//                html += '</div>';
+//                html += '<div>' + this.albumname + '</div>';
+//                if (!selectOnClick)
+//                    html += '</a>';
+//                html += '</td>';
+
+//                currentRowImagesCount++;
+
+//                if (currentRowImagesCount == imagesPerColumn) {
+//                    html += "</tr><tr>";
+//                    currentRowImagesCount = 0;
+//                }
+//            });
+
+//            html += '</tr>';
+
+//            $(this).html(html);
+
+//            if (selectOnClick) {
+//                $(this).find('img').click(function () {
+//                    $(this).toggleClass('selectedImage').toggleClass('unselectedImage');
+//                });
+//            }
+//        });
+//    };
+//})(jQuery);
