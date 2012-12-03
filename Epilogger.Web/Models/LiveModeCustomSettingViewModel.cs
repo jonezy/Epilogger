de16618fc3c1ce76cbb6712ee54c1819ba367319
Mike Motschenbacher <MikeM@Epilogger.com>
2012-12-03 09:03:00 -0500

    commit more live mode

---
+++ Epilogger.Web/Models/LiveModeCustomSettingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Epilogger.Data;
+using Twitterizer;
+
+namespace Epilogger.Web.Models {
+    public class LiveModeCustomSettingViewModel {
+        public int EventId { get; set; }
+
+        [StringLength(7, ErrorMessage = "must be 7 characters in length (#FFF000)")]
+        [DisplayName("Background colour")]
+        public string Background{ get; set; }
+
+        [StringLength(7, ErrorMessage = "must be 7 characters in length (#FFF000)")]
+        [DisplayName("Footer text colour")]
+        public string FooterTextColor { get; set; }
+
+        [StringLength(7, ErrorMessage = "must be 7 characters in length (#FFF000)")]
+        [DisplayName("Twitter username colour")]
+        public string TwitterUserNameColour { get; set; }
+
+        [StringLength(7, ErrorMessage = "must be 7 characters in length (#FFF000)")]
+        [DisplayName("Link colour")]
+        public string LinkColour { get; set; }
+
+        [DisplayName("Logo")]
+        public string CompanyLogo { get; set; }
+
+        [DisplayName("Sponsor Logo")]
+        public string SponsorLogo{ get; set; }
+    }
+}
---
+++ Epilogger.Web/Public/js/FileUploader.js
@@ -0,0 +1,19 @@
+﻿head.ready(function() {
+
+    var button = $('#uploadFile')[0];
+    var uploader = new qq.FileUploader({
+        element: button,
+        allowedExtensions: ['jpg', 'jpeg', 'png', 'gif'],
+        sizeLimit: 2147483647, // max size
+        action: '/Event/UploadFile',
+        multiple: true,
+        uploadButtonText: 'upload',
+        onComplete: function(id, fileName, responseJSON) {
+            $("#theFileImage").attr("src", responseJSON.imageurl);
+            $("#FileImage").val(responseJSON.imageurl);
+            $("#DisplayFileImage").val(responseJSON.imageurl);
+        }
+    });
+   
+
+});
